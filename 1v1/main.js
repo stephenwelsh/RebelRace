e024a9a4b2297f8a7edb8c3a1702b0b01d87adda
Stephen Welsh <stephen.welsh@unifiedfx.com>
2019-11-09 19:23:40 +0000

    Optional loop, remove canvas and hide before/after

--- 1v1/main.js
+++ 1v1/main.js
@@ -4,6 +4,7 @@ var urlParams = new URLSearchParams(window.location.search);
 var winner = urlParams.get('winner') || "ScottishRebel67";
 var looser = urlParams.get('looser');
 var referee = urlParams.get('referee');
+var loop = urlParams.get('loop') == "true";
 var timeOffset = urlParams.get('offset');
 if(timeOffset) timeOffset = parseInt(timeOffset);
 var hideReferee = urlParams.get('hideReferee') == "true";
@@ -25,38 +26,48 @@ Promise.all(results).then(r => {
     var winnerUser = r[1][0];
     var winner = document.getElementById('winner');
     targets.push(new Promise(resolve => {
-        winner.onload = resolve;        
+        winner.onload = ()=>{
+            resolve();
+            new VideoOverlay({
+                keyframes: r[0]["winner"],
+                timeOffset: timeOffset,
+                source: 'source',
+                target: 'winner'
+            });        
+        };
     }));
-    new VideoOverlay({
-        keyframes: r[0]["winner"],
-        timeOffset: timeOffset,
-        source: 'source',
-        canvas: 'canvas1',
-        target: 'winner',
-        clip: true
-    });
+    winner.src = winnerUser.avatarUrl;
     if(r.length > 2){
         var looserUser = r[2][0];
         var looser = document.getElementById('looser');
         targets.push(new Promise(resolve => {
-            looser.onload = resolve;        
+            looser.onload = ()=>{
+                resolve();
+                new VideoOverlay({
+                    keyframes: r[0]["looser"],
+                    timeOffset: timeOffset,
+                    source: 'source',
+                    target: 'looser'
+                });                    
+            };    
         }));
         looser.src = looserUser.avatarUrl;
     }
-    new VideoOverlay({
-        keyframes: r[0]["looser"],
-        timeOffset: timeOffset,
-        source: 'source',
-        canvas: 'canvas2',
-        target: 'looser',
-        clip: true
-    });
+    else
+    {
+        new VideoOverlay({
+            keyframes: r[0]["looser"],
+            timeOffset: timeOffset,
+            source: 'source',
+            target: 'looser'
+        });    
+    }
     var referee = document.getElementById('referee');
     if(!hideReferee){
         if(r.length > 3){
             var refereeUser = r[3][0];
             targets.push(new Promise(resolve => {
-                referee.onload = resolve;        
+                referee.onload = resolve;
             }));
             referee.src = refereeUser.avatarUrl;
         }
@@ -64,17 +75,27 @@ Promise.all(results).then(r => {
             keyframes: r[0]["referee"],
             timeOffset: timeOffset,
             source: 'source',
-            canvas: 'canvas3',
-            target: 'referee',
-            clip: r.length > 3
+            target: 'referee'
         });
     }
     else referee.removeAttribute('src');
     Promise.all(targets).then(r =>{
         var source = document.getElementById('source');
-        source.setAttribute('src', video + ".mp4");
         var targets = document.querySelectorAll("img.target");
-        targets.forEach(t => t.style.opacity = 1);
+        source.addEventListener('playing', (event) => {
+            targets.forEach(t => {
+                t.style.opacity = 1;
+            });
+            source.style.opacity = 1;
+        });
+        source.addEventListener('ended', (event) => {
+            targets.forEach(t => t.style.opacity = 0);
+            source.style.opacity = 0;
+        });
+        // ['playing','ended','play','loadeddata'].forEach(evt => source.addEventListener(evt,event => {
+        //     console.log(event);
+        // }));
+        source.setAttribute('src', video + ".mp4");
+        if(loop) source.setAttribute('loop','');
     });
-    winner.src = winnerUser.avatarUrl;
 });
--- 1v1/overlay.js
+++ 1v1/overlay.js
@@ -7,76 +7,42 @@ var VideoOverlay = function(options){
     this.options.timeTollerance = this.options.timeTollerance || 8;
     this.options.scale = this.options.scale || 1;
     this.options.source = this.options.source || 'source';  // Video source to overlay target on
-    this.options.canvas = this.options.canvas || 'canvas';  // Canvas to draw overlay on
     this.options.target = this.options.target || 'target';  // Target to overlay on top of source
     this.options.targetOffsetX = this.options.targetOffsetX || 0;
     this.options.targetOffsetY = this.options.targetOffsetY || 0;
     this.options.keyframes = this.options.keyframes || { times: [], position: [] };
-    this.options.keyframeSource = this.options.keyframeSource || "DancingGorrila.json";
     this.video = document.getElementById(this.options.source) || document.getElementsByTagName('video')[0];
-    this.canvas = document.getElementById(this.options.canvas) || document.getElementsByTagName('canvas')[0];
     this.target = document.getElementById(this.options.target)|| document.getElementsByTagName('img')[0];
-    if(this.canvas) this.ctx = this.canvas.getContext("2d");
     this.keyframeIndex = -1;
-    this.start = null;
-    this.lastFrametime = -1;
-    if(this.video){
-        var self = this;
-        this.video.addEventListener('playing', (event) => {
-            self.keyframeIndex = 0;
-            self.start = this.lastFrametime;
-            console.log('Video started playing or looped');
-        });
-        window.requestAnimationFrame(this.frameCallback.bind(this));
-    }
+    this.target.originalWidth = this.target.clientWidth;
+    this.target.originalHeight = this.target.clientHeight;
+
+    window.requestAnimationFrame(this.frameCallback.bind(this));
 };
 
 VideoOverlay.prototype = {
-    drawFrame: function(ctx){
-        var x = this.targetX - this.target.width/2 * this.scaleX;
-        var y = this.targetY - this.target.height/2 * this.scaleY;
-        var w = this.target.width * this.scaleX;
-        var h = this.target.height * this.scaleY;
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        if(this.options.clip){
-            ctx.beginPath();
-            // TODO: If clip is a function then call passing in the current ctx and Overlay instance to allow dynamic clipping
-            ctx.arc(x+w/2, y+h/2, w/2, 0, Math.PI * 2);
-            ctx.fill();
-            ctx.closePath();
-            ctx.globalCompositeOperation = 'source-in';    
-        }
-        ctx.drawImage(this.target, x, y, w, h);
-        ctx.globalCompositeOperation = 'source-over';
-    },
     updateTarget: function(){
-        var x = this.targetX - this.target.width/2 * this.scaleX;
-        var y = this.targetY - this.target.height/2 * this.scaleY;
+        var w = this.target.originalWidth  * this.scaleX;
+        var h = this.target.originalHeight * this.scaleY;
+        var x = this.targetX - w/2;
+        var y = this.targetY - h/2;
         this.target.style.left = x + "px";
         this.target.style.top = y + "px";
-    },
-    resizeCanvas: function(w,h){
-        if(this.canvas && (this.canvas.width != w || this.canvas.height != h)){
-            this.canvas.width = w;
-            this.canvas.height = h;
-        }
-    }
+        this.target.style.width = w + "px";
+        this.target.style.height = h + "px";
+     },
 };
 
 VideoOverlay.prototype.frameCallback = function(timestamp){
-    this.lastFrametime = timestamp;
-    if(this.video) this.resizeCanvas(this.video.offsetWidth, this.video.offsetHeight);
-    if(this.start && this.video && !this.video.paused && !this.video.ended){
-        var progress = timestamp - this.start + this.options.timeOffset;
+    if(this.video && !this.video.paused && !this.video.ended){
         var lastIndex = this.keyframeIndex;
         var lastVideoTime = this.videoTime;
         this.videoTime = this.video.currentTime;
         if(lastVideoTime != this.videoTime && this.options.keyframes.times.length > 0){
-            var keyframeTime = -1;
             var timeMap = this.options.keyframes.times.map((f,i) => {
                 return {
                     index: i,
-                    delta: Math.abs(progress - f * 1000)
+                    delta: Math.abs(this.videoTime - f)
                 };
             });
             timeMap.sort((a,b) => a.delta - b.delta);
@@ -89,13 +55,11 @@ VideoOverlay.prototype.frameCallback = function(timestamp){
                     this.scaleY = this.options.scale * this.video.clientHeight / this.video.videoHeight;
                     this.targetX = ((position[1] + this.options.targetOffsetX) * this.scaleX).toFixed(0);
                     this.targetY = ((position[2] + this.options.targetOffsetY) * this.scaleY).toFixed(0);
-                    // this.drawFrame(this.ctx);
                     this.updateTarget();
                 }
             }
         }
         if(this.options.keyframes.times.length <= this.keyframeIndex) this.keyframeIndex = 0;
     }
-    // Bind with this instance
     window.requestAnimationFrame(this.frameCallback.bind(this));
 };
